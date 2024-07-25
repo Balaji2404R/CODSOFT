6f8c1e6b617c53041175668244c98e739e9cf9db
Balaji2404R <146181220+Balaji2404R@users.noreply.github.com>
2024-07-25 20:30:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const display = document.getElementById('display');
+
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function updateDisplay() {
+    display.innerText = currentOperand;
+}
+
+function appendNumber(number) {
+    if (number === '0' && currentOperand === '0') return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function deleteNumber() {
+    currentOperand = currentOperand.toString().slice(0, -1);
+    updateDisplay();
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNumber(button.innerText);
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        chooseOperation(button.innerText);
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+    compute();
+});
+
+clearButton.addEventListener('click', button => {
+    clearDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    deleteNumber();
+});
